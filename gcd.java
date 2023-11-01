1e8717996ebde89234b08b2a1cd37b2eb82de85f
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2023-11-01 21:14:03 +0530

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -1,26 +1,19 @@
 import java.util.Scanner;
 
-public class gcd {
+public class q9 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n,k;
-        n=sc.nextInt();
-        k=sc.nextInt();
-        int count=0,org=n;
-        for(;n>0;n/=10){
-            count++;
-        }
-        int num=0;
-        k=k%count;
-        if(k<0){
-            k=k+count;
-        }
-        int div=(int)Math.pow(10,k);
-        int quo=org/div;
-        int rem=org%div;
-        int mul=(int)Math.pow(10,(count-k));
-        num=(rem*mul)+quo;
-        
-        System.out.println(num);
+        int n1,n2;
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        int gcd=0,lcm;
+        for(int i=1;i<=Math.min(n1,n2);i++){
+            if(n1%i==0 && n2%i==0){
+                gcd=i;
+                
+            }
+                
+        }   
+        System.out.println(gcd);
     }
 }
