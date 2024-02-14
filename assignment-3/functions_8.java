d1bc55d9f528dedb0212b65f2d90e90dfb5450a2
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2024-02-14 22:34:27 +0530

    update

---
+++ assignment-3/functions_8.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class functions_8 {
+
+    static int largestElement(int[] arr, int n) {
+        int max = arr[0];
+
+        // sorting the array first
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+
+        }
+        return max;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 5, 4, 6, 4, 2, 8 };
+        int n = arr.length;
+        int maximum = largestElement(arr, n);
+        System.out.println("maximum number is " + maximum);
+
+    }
+}
